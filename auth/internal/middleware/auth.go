8dbff4dfa8eccdcead21671b4c34ae12b304c70f
princebhati <princebhati074@gmail.com>
2025-06-14 16:40:48 +0530

    bug in middleware

--- auth/internal/middleware/auth.go
+++ auth/internal/middleware/auth.go
@@ -2,12 +2,15 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/go-chi/jwtauth/v5"
 )
 
 func RequiredAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		if ctx.Value("jwt") == nil {
+		_, claims, _ := jwtauth.FromContext(r.Context())
+
+		if claims == nil {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
